396c2732791c1e8006e5c269c3ade4ccb4956d9f
Hüdai ERBAŞ <47556170+hudaierbas@users.noreply.github.com>
2021-10-01 14:11:18 +0300

    Add files via upload

---
+++ pdf_compressor2.py
@@ -0,0 +1,160 @@
+# download Ghostscript https://www.ghostscript.com/download.html
+# change the gswin64c.exe path at subproccess.popen
+
+from __future__ import print_function
+import os
+import subprocess
+import time
+import sys
+import signal
+from datetime import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# compressed_file_list path
+parser.add_argument("-cl", "--compressed_list",
+                    required=False, type=str, help="sıkıştırılma işlemi yapılmış dosya isimlerinin bulunduğu txt dosyası")
+
+# day difference dd format
+parser.add_argument("-d", "--day",
+                    required=False, type=int, help="sıkıştırılma işlemi yapılmayacak son gün sayısı")
+
+args = parser.parse_args()
+
+lines = []
+if args.compressed_list != None:
+    file = open(args.compressed_list, "r")
+    lines = file.read().splitlines()
+
+
+date = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+
+#main_dir = "\\\\depoerp\\\\ErpDosyaDepo\\\\TempEmployee"
+main_dir = "C:/Users/herbas/Desktop/pdf"
+
+t_start_time = time.time()
+file_size_kb = 0
+compressed_file_size_kb = 0
+count = 0
+
+
+def handler(signum, frame):  # handle terminate
+    print("------------------------------------------")
+    error_log(1)
+    sys.exit("Sıkıştırma durduruldu.")
+
+
+signal.signal(signal.SIGINT, handler)  # crtl + c  ==> call handler
+
+compressd_file_logs = open("compressed_file_list_%s.txt" % date, "w")
+
+
+def compressed_file_list(compressed_file):
+    compressd_file_logs.write("%s\n" % compressed_file)
+
+
+errorList = []
+log = open("error_log_%s.txt" % date, "w")
+log.write("date: %s \n" % date)
+log.write("main_dir: %s \n" % main_dir)
+
+
+def error_log(p):  # create log
+    if p == 1:
+        log.write("subprocess terminated \n")
+        log.write("files: %s  \n" % count)
+        log.write("total file size: %s mb \n" % file_size_mb)
+        log.write("compressed file size: %s mb \n" % compressed_file_size_mb)
+        log.write("compress percentage: %s \n" % compress_perc)
+
+        if len(errorList) > 0:
+            for error in errorList:
+                log.write("------------------------------------------\n")
+                log.write("folder: %s \n" % error[0])
+                log.write("file: %s \n" % error[1])
+                log.write("error: %s \n" % error[2])
+                log.write("------------------------------------------\n")
+
+    if p == 2:
+        log.write("files: %s  \n" % count)
+        log.write("total file size: %s mb \n" % file_size_mb)
+        log.write("compressed file size: %s mb \n" % compressed_file_size_mb)
+        log.write("compress percentage: %s \n" % compress_perc)
+
+        if len(errorList) > 0:
+            for error in errorList:
+                log.write("------------------------------------------\n")
+                log.write("folder: %s \n" % error[0])
+                log.write("file: %s \n" % error[1])
+                log.write("error: %s \n" % error[2])
+                log.write("------------------------------------------\n")
+
+
+def check_date(filename):
+    file_date = datetime.fromtimestamp(
+        os.path.getmtime(filename))
+
+    max_day_difference = args.day
+
+    day_difference = (datetime.now() - file_date).days
+
+    if day_difference < max_day_difference:
+        return "break"
+
+
+for root, dirs, files in os.walk(main_dir):
+    for file in files:
+        if file.endswith(".pdf"):
+            filename = os.path.join(root, file)
+            folder = os.path.basename(os.path.dirname(filename))
+
+            if args.compressed_list != None:
+                if file in lines:
+                    break
+
+            if args.day != None:
+                if check_date(filename) == "break":
+                    break
+
+            print("Klasör: %s" % folder)
+            print("%s sıkıştırılıyor..." % file)
+            start_time = time.time()
+
+            arg1 = '-sOutputFile=' + "compressed_" + file
+            p = subprocess.Popen(['C:/Program Files/gs/gs9.55.0/bin/gswin64c.exe', '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
+                                 '-dPDFSETTINGS=/ebook',  '-dNOPAUSE', '-dBATCH', '-dQUIET', str(arg1), filename], cwd=root, stdout=subprocess.PIPE)
+
+            if p.communicate()[1] != None:
+                errorList.append([folder, file, p.communicate()[1]])
+
+            count += 1
+            compressed_file_path = os.path.join(root, "compressed_" + file)
+            compressed_file_size_kb += os.path.getsize(compressed_file_path)
+            file_size_kb += os.path.getsize(filename)
+
+            os.remove(filename)
+            os.rename(compressed_file_path, filename)
+
+            compressed_file_list(file)
+
+            print("işlem süresi:  %s saniye " %
+                  ("{:.2f}".format(time.time() - start_time)))
+            print("------------------------------------------")
+
+print("Toplam işlem süresi:  %s saniye " %
+      ("{:.2f}".format(time.time() - t_start_time)))
+
+file_size_mb = "{:.2f}".format(file_size_kb / (1024*1024))
+compressed_file_size_mb = "{:.2f}".format(
+    compressed_file_size_kb / (1024*1024))
+
+compress_perc = "{:.2f}".format(
+    ((file_size_kb - compressed_file_size_kb) / file_size_kb) * 100)
+
+print("Sıkıştırılan dosya: %s adet" % count)
+print("Toplam boyut: %s mb" % file_size_mb)
+print("Sıkıştırılmış toplam boyut: %s mb" % compressed_file_size_mb)
+print("Sıkıştırma oranı: %s" % compress_perc)
+
+error_log(2)
